8a0cfef2c68851e71fb43b292dfaa4089e2b58f9
maria <m.heredero@alumnos.upm.es>
2026-04-06 17:47:31 +0200

    avanzo poco a poco

--- archonfutbol/principal.cpp
+++ archonfutbol/principal.cpp
@@ -1,4 +1,8 @@
 // archonfutbol.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//controles pantalla de seleccion: 
+// 1/2/3 se elige el equipo LOCAL (Barsa / Atletico / Madrid)
+// 4/5/6 equipo VISITANTE
+// ENTER empezar la partida
 #include <iostream>
 
 int main()
