1296a2e5a7c3752a0aa9f026708a17befe04d6db
maria <m.heredero@alumnos.upm.es>
2026-04-06 21:57:38 +0200

    avance configuracion de equipo

--- archonfutbol/principal.cpp
+++ archonfutbol/principal.cpp
@@ -3,7 +3,14 @@
 // 1/2/3 se elige el equipo LOCAL (Barsa / Atletico / Madrid)
 // 4/5/6 equipo VISITANTE
 // ENTER empezar la partida
+
+//Controles de la partida:
+//  Clic izq: seleccionar pieza / moverla
+// ESC:  volver a selección
 #include <iostream>
+#include "TableroGL.h"
+#include "ETSIDI.h"
+#include <glut.h>
 
 int main()
 {
