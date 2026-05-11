cacb7dfed75272e8ecbaa97f0938b620bc9fa9c7
Sofia18-10 <sofia.jara.quinones@alumnos.upm.es>
2026-05-11 22:28:36 +0200

    Añadido setter de posición al combatiente
    Método público para mover al combatiente a una posición concreta.
    Declarado en el .h e implementado en el .cpp porque puede crecer
    cuando añada validación de limites de la arena.

--- arena_demo_sofia/Combatiente.cpp
+++ arena_demo_sofia/Combatiente.cpp
@@ -3,4 +3,12 @@
 Combatiente::Combatiente(float x, float y, float z, float r, float g, float b)
 	: _x{ x }, _y{ y }, _z{ z }, _r{ r }, _g{ g }, _b{ b }
 {
+
+}
+
+void Combatiente::posicion(float x, float y, float z)
+{
+	_x = x;
+	_y = y;
+	_z = z;
 }
