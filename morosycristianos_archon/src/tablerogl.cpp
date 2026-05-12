e334900df1b6cbb52dd7eb8e389714719618d3e8
maria <m.heredero@alumnos.upm.es>
2026-05-12 18:44:10 +0200

    defino en el tablerogl.cpp el constructor y los métodos de conversión de coordenadas

--- morosycristianos_archon/src/tablerogl.cpp
+++ morosycristianos_archon/src/tablerogl.cpp
@@ -1,1 +1,38 @@
+//aquí se va a dibujar el tablero. 
+//Se recorren las 81 casillas del tablero y por cada una decide si es clara(par) u oscura (impar), según el tipo de casillas elige el par de colores concreto y luego dibuja un rectángulo relleno con el color
+//Por último se dibuja la cuadrícula negra encima
+
 #include "tablerogl.h"
+#include <iostream>
+using namespace std;
+
+Tablerogl::Tablerogl(Tablero* pb):m_tablero(pb)
+{
+	N = pb->getSize();//Siempre va a ser nueve, pero para asegurarnos mejor leerlo directamente de nuestra clase tablero
+	ancho = 0.13f;//ancho de una casilla en unidades Opengl
+	dist = 1.8;//distancia que hay de la cámara al tablero
+	//el centro del tablero es donde va a apuntar la cámara
+	//x positivo crece hacia la derecha, y negativo crece hacia abajo
+	centro_x = N * ancho / 2.0;
+	centro_y = -N * ancho / 2.0;
+	centro_z = 0.0;
+
+	xcasilla_sel = -1;//todavía no hay casilla seleccionada
+	ycasilla_sel = -1;
+
+	leftButton = rightButton = midButton = false;
+	controlKey = shiftKey = false;
+}
+
+void Tablerogl::cell2center(int casilla_x, int casilla_y, float& glx, float& gly)
+{
+	glx = casilla_y * ancho + ancho / 2.0f;
+	gly = -casilla_x * ancho - ancho / 2.0f;
+}
+
+void Tablerogl::world2cell(double x, double y, int& casilla_x, int& casilla_y)
+{
+	casilla_x = (int)(fabs(y / ancho));
+	casilla_y = (int)(x / ancho);
+}
+
