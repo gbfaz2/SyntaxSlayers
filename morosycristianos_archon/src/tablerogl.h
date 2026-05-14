4def5d4bbc7ca692689eb0e383fd7346ff026af5
albi2214 <alba.dconde@alumnos.upm.es>
2026-05-15 00:06:57 +0200

    Implementacion de clase gestorturnos, falta llamar a las funciones desde el main para hacerlo funcional y faltan también otras dos clases gestoras para desacoplar todo

--- morosycristianos_archon/src/tablerogl.h
+++ morosycristianos_archon/src/tablerogl.h
@@ -1,6 +1,8 @@
 // lo usamos para separar la lógica del dibujo. Primero probaremos a hacer un tablero vacío con un aspecto visual del Archon original
 #pragma once
 #include "Tablero.h"
+
+
 //creo las enumeraciones con las variables del raton y de las teclas especiales para signarles el mismo valor que tenemos en el freeglut.h
 enum {MOUSE_LEFT_BUTTON, MOUSE_MIDDLE_BUTTON, MOUSE_RIGHT_BUTTON};
 enum {KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT};
@@ -11,7 +13,7 @@ protected:
 	float ancho;
 	int N;
 	Tablero* m_tablero;
-
+	
 	double centro_x, centro_y, centro_z;
 	double dist;
 
