7d6d8a38ca6a0a42d1e367fe44d348ed0f5da62d
maria <m.heredero@alumnos.upm.es>
2026-05-17 00:36:12 +0200

    actualización de coordinador.h

--- morosycristianos_archon/src/coordinador/headers/Coordinador.h
+++ morosycristianos_archon/src/coordinador/headers/Coordinador.h
@@ -1,19 +1,35 @@
 #pragma once
+#include "estadojuego.h"
+#include "menu.h"
+#include "pantallabatalla.h"
+#include "tablero.h"
+#include "tablerogl.h"
 
 class Coordinador
 {
 	// DEFINIMOS LOS ESTADOS AQUÍ MISMO PARA NO DEPENDER DE OTROS ARCHIVOS
-	enum Estado { INTRO, MENU, TABLERO, ARENA, PAUSA, RANKING, GAMEOVER };
-	Estado estado{ INTRO }; // ESTADO INICIAL
+	EstadoJuego     estado{ EstadoJuego::INTRO };
+
+	int             anchoVentana{ 800 };
+	int             altoVentana{ 600 };
+
+	PantallaIntro   pantallaIntro;
+	MenuPrincipal   menuPrincipal;
+	PantallaDestino pantallaDestino;
+	ConfigPartida   configuracion;
+	Tablero* pTablero{ nullptr };
+	Tablerogl* pTablerogl{ nullptr };
 
 public:
 	Coordinador() = default; // CONSTRUCTOR
-	virtual ~Coordinador() = default; // DESTRUCTOR
+	~Coordinador(); // DESTRUCTOR
 
 	void inicializa(); // SETUP INICIAL
+	void dibuja(); // PINTA SEGÚN ESTADO
 	void tecla(unsigned char key); // GESTIÓN TECLADO
 	void tecla_especial(int key); // GESTIÓN FLECHAS
 	void mueve(double dt); // ACTUALIZA LÓGICA
-	void dibuja(); // PINTA SEGÚN ESTADO
 	void raton(int button, int state, int x, int y); // GESTIÓN RATÓN
+	void ratonMovido(int x, int y);
+	void redimensionar(int ancho, int alto);
 };
