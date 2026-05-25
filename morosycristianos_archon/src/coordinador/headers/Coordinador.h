969438d25848a139ae68a5cc369e7e746277cad8
gbfaz2 <gabrif894@gmail.com>
2026-05-25 23:25:22 +0200

    primer intento de ia fallido.

--- morosycristianos_archon/src/coordinador/headers/Coordinador.h
+++ morosycristianos_archon/src/coordinador/headers/Coordinador.h
@@ -12,6 +12,12 @@
 #include "GestorHechizos.h"
 #include "GestorInput.h"
 #include "SpriteRey.h"
+<<<<<<< Updated upstream
+=======
+#include "GestorPartida.h"
+#include "MinimaxTablero.h"
+
+>>>>>>> Stashed changes
 
 class Coordinador
 {
@@ -41,6 +47,9 @@ class Coordinador
 
 	Pieza* _pAtacanteCombate{ nullptr };  // pieza atacante del combate actual
 	Pieza* _pDefensoraCombate{ nullptr }; // pieza defensora del combate actual
+
+	MinimaxTablero _minimax{ 2 }; // IA CON PROFUNDIDAD 2
+	bool _iaCalculando{ false }; // EVITA CALCULAR VARIAS VECES POR FRAME
 
 public:
 	Coordinador() = default;
