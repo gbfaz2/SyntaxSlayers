46ca5c3d26826c8f4888e37b8c764f7c620f3b02
Sofia18-10 <sofia.jara.quinones@alumnos.upm.es>
2026-05-25 12:39:21 +0200

    Retoque de la nueva clase
    He puesto arriba lo private y debajo lo public

--- morosycristianos_archon/src/coordinador/headers/dibujapersonajes.h
+++ morosycristianos_archon/src/coordinador/headers/dibujapersonajes.h
@@ -1,10 +1,7 @@
 #pragma once
 #include "ETSIDI.h"
 
-
-
 //solo para miliciano de momento para prueba. Si funciona, se meteran en esta clase para todos los personajes
-
 
 enum class EstadoPersonaje {
     IDLE = 0,  // fila 1 del spritesheet
@@ -12,7 +9,12 @@ enum class EstadoPersonaje {
     HURT = 8, // fila 3
 };
 
-class dibujapersonajes {
+class dibujapersonajes 
+{
+    //funcion de libreria etsidi para animación
+    static const int MAX_MILICIANOS = 14;
+    ETSIDI::SpriteSequence* _milicianos[MAX_MILICIANOS];
+
 public:
     dibujapersonajes();
     ~dibujapersonajes();
@@ -22,9 +24,4 @@ public:
 
     void miliciano(float x, float y, float size,
         EstadoPersonaje estado, int indice, bool enMovimiento = false);
-private:
-    //funcion de libreria etsidi para animación
-    static const int MAX_MILICIANOS = 14;
-    ETSIDI::SpriteSequence* _milicianos[MAX_MILICIANOS];
-
 };
