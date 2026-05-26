e524d560a100224537771e58aa070e9c311a06ba
albi2214 <alba.dconde@alumnos.upm.es>
2026-05-26 03:04:17 +0200

    continuacion personajes tablero, falta ajustar bastantes cosas, he metido imagenes, editado dibujapersonajes y dibujatablero, y tablerogl

--- morosycristianos_archon/src/coordinador/headers/dibujapersonajes.h
+++ morosycristianos_archon/src/coordinador/headers/dibujapersonajes.h
@@ -1,7 +1,20 @@
 #pragma once
 #include "ETSIDI.h"
+#include "tablero.h"
 
 //solo para miliciano de momento para prueba. Si funciona, se meteran en esta clase para todos los personajes
+
+//falta completar para el resto de personajes
+
+enum class TipoPersonaje {
+    MILICIANO,
+    EMIR,
+    INFILTRADO,
+    GUARDIA_NEGRA,
+    JINETE_BEREBER,
+    TOTAL  // siempre al final, cuenta cuántos hay
+};
+
 
 enum class EstadoPersonaje {
     IDLE = 0,  // fila 1 del spritesheet
@@ -11,9 +24,14 @@ enum class EstadoPersonaje {
 
 class dibujapersonajes 
 {
+
+    //PENDIENTE: SWITCH CASE?? O BUCLE
     //funcion de libreria etsidi para animación
-    static const int MAX_MILICIANOS = 14;
-    ETSIDI::SpriteSequence* _milicianos[MAX_MILICIANOS];
+    static const int MAX_SPRITES = 14; //MAX POR TIPO (MILICIANOS)
+    ETSIDI::SpriteSequence* _sprites[(int)TipoPersonaje::TOTAL][MAX_SPRITES];
+
+    //RUTA IMAGEN PARA CADA PERSONAJE
+    static const char* rutaImagen(TipoPersonaje tipo);
 
 public:
     dibujapersonajes();
@@ -22,6 +40,11 @@ public:
     void init();
     void update();
 
-    void miliciano(float x, float y, float size,
+    //método para dibujar miliciano
+    void dibujar(TipoPersonaje tipo, float x, float y, float size,
         EstadoPersonaje estado, int indice, bool enMovimiento = false);
+    
+    // Helper: convierte TipoPieza + BandoPieza a TipoPersonaje
+    static TipoPersonaje tipoDesdePieza(TipoPieza pieza, BandoPieza bando);
+
 };
